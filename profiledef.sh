0e61fd583449f4081a7af8e9636dbe635185be31
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-05-01 18:42:49 +0300

    version&packages

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="HelwanAl-AmalCinnamonV3.1"
+iso_name="HelwanAl-AmalCinnamonV3.2"
 iso_label="Helwan_Alamal_Cinnamon_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="helwanlinux <helwanlinux@gmail.com>"
 iso_application="Helwan Linux Live/Rescue DVD"

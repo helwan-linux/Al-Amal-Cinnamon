b313bd8edaa2a178c3335242ce9c5d3be152cebd
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-05-12 10:23:55 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -26,4 +26,5 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
+  ["/root/customize_airootfs.sh"]="0:0:755"
 )

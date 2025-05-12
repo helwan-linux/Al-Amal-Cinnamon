b7aacc2df52677e2ad112304dfd17fb50238cd50
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-05-12 10:17:09 +0300

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Enable fa_IR and ur_PK locales for future user selection
+# Ensure fa_IR.UTF-8 is enabled (مفيهاش مشكلة)
 sed -i '/fa_IR.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+
+# Ensure ur_PK locale line is correct and enabled
+# لو السطر موجود بشكل غلط (ur_PK UTF-8) يعدله صح (ur_PK.UTF-8 UTF-8)
+sed -i 's/^#\?ur_PK UTF-8$/ur_PK.UTF-8 UTF-8/' /etc/locale.gen
 sed -i '/ur_PK.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 
-# Generate the locales (بدون لمس /etc/locale.conf)
+# Generate the locales
 locale-gen
-
